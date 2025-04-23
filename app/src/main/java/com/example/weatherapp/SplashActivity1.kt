61813b1fff2984cb05ac4139f9012e0fd7d03d37
longdn81 <daonhatlong81@gmail.com>
2025-04-23 22:16:04 +0700

    update Splash Screen

--- app/src/main/java/com/example/weatherapp/SplashActivity1.kt
+++ app/src/main/java/com/example/weatherapp/SplashActivity1.kt
@@ -1,23 +1,25 @@
 package com.example.weatherapp
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
 
 class SplashActivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash1)
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this,MainActivity:: class.java)
-            startActivity(intent)
+
+        lifecycleScope.launch {
+            delay(3000)
+            startActivity(Intent(this@SplashActivity1, MainActivity::class.java))
             finish()
-        } , 3000)
+        }
     }
 }
